bf581d6d7928a1726c6e032126337eecc42b1929
205277097 <205277097@qq.com>
2024-03-01 21:57:12 +0800

    edit CrossOrigin

--- src/main/java/com/tencent/wxcloudrun/controller/CounterController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/CounterController.java
@@ -7,21 +7,17 @@ import com.tencent.wxcloudrun.dto.CounterRequest;
 import com.tencent.wxcloudrun.model.Counter;
 import com.tencent.wxcloudrun.service.CounterService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.List;
 
-import org.springframework.web.bind.annotation.RequestMapping;
 /**
  * counter控制器
  */
 @RestController
-
+@CrossOrigin
 public class CounterController {
 
   final CounterService counterService;
