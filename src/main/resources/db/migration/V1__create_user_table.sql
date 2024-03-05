513de11f43a5f75686bf68f1d36f7fd5e0a31076
205277097 <205277097@qq.com>
2024-03-05 16:20:13 +0800

    add

--- src/main/resources/db/migration/V1__create_user_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `user` (
-     `id` VARCHAR(32) NOT NULL PRIMARY KEY,
-     `username` VARCHAR(32) NOT NULL COLLATE 'utf8_general_ci',
-     `password` VARCHAR(100) NOT NULL COLLATE 'utf8_general_ci',
-     `email` VARCHAR(100) NULL COLLATE 'utf8_general_ci',
-     `gender` VARCHAR(100) NULL COLLATE 'utf8_general_ci'
-)
